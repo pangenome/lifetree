997db5c49d294b23910253985a66bf5f1d6e38f5
Erik Garrison <erik.garrison@gmail.com>
2024-06-05 11:02:28 -0500

    get it going on TACC/stampede3

--- vgp/wfmash-v0.14.0-map-align.sh
+++ vgp/wfmash-v0.14.0-map-align.sh
@@ -4,10 +4,7 @@
 #SBATCH --time=2-00:00:00
 
 # break on error
-set -euo pipefail
-
-# our wfmash binary
-wfmash=$WORK/micromamba/bin/wfmash
+set -euox pipefail
 
 seqs=$1
 jobs=$2
@@ -15,7 +12,16 @@ id=$3
 out=$4
 
 cpus=$SLURM_CPUS_PER_TASK
+# set to max of parameter defined and available cpus
+cpus_for_mapping=$(($cpus > $5 ? $5 : $cpus))
+cpus_for_aligning=$(($cpus > $6 ? $6 : $cpus))
+echo "cpus for mapping: $cpus_for_mapping"
+echo "cpus for aligning: $cpus_for_aligning"
+
 time=/usr/bin/time
+fmt="%C\n%Us user %Ss system %P cpu %es total %MKb max memory"
+timer=$(which time)
+
 ls -l $seqs
 mkdir -p $out
 
@@ -23,34 +29,35 @@ spec=$(head -$id $jobs | tail -1)
 target=$(echo $spec | awk '{print $1}')
 query=$(echo $spec | awk '{print $2}')
 
-echo "wfmash mapping $id = $target against $query on $(hostname) with $cpus CPUs at $(date +%s) / $(date) with $wfmash "
-$wfmash --version
+echo "wfmash mapping $id = $target against $query on $(hostname) with $cpus CPUs at $(date +%s) / $(date) with $(which wfmash)"
+wfmash --version
 
 map_out=$out/$id.$target.map
-$time -vv $wfmash -t $cpus \
-      -m \
-      -T $target \
-      -Q $query \
-      -Y '#' \
-      -n 1 \
-      -k 19 \
-      -p 70 \
-      -s 5k \
-      -c 20k \
-      $seqs \
-      >$map_out.paf \
-      2>$map_out.log \
-    && touch $map_out.ok
+
+( $timer -f "$fmt" \
+    wfmash -t $cpus_for_mapping \
+           -m \
+           -T $target \
+           -Q $query \
+           -Y '#' \
+           -n 1 \
+           -k 19 \
+           -p 70 \
+           -s 5k \
+           -c 20k \
+           $seqs \
+           >$map_out.paf ) 2>$map_out.log
+touch $map_out.ok
 
 echo "wfmash aligning $id = $target against $query on $(hostname) with $cpus CPUs at $(date +%s) / $(date)"
 
 aln_out=$out/$id.$target.aln
-$time -vv $wfmash -t $cpus \
-      -i $out/$target.id_$id.map.paf \
-      $seqs \
-      >$aln_out.paf \
-      2>$aln_out.log \
-    && touch $aln_out.ok
+( $timer -f "$fmt" \
+    wfmash -t $cpus_for_aligning \
+           -i $map_out.paf \
+           $seqs \
+           >$aln_out.paf ) 2>$aln_out.log
+touch $aln_out.ok
 
 echo "zipping $map_out.paf and $aln_out.paf"
 pigz -f $map_out.paf $aln_out.paf
